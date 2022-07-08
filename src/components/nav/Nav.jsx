c56fdd6efba2796c9c64da214ab1de437a9afdf6
OzanOcak <ozanocak@gmail.com>
2022-07-08 12:34:00 -0400

    react hook  scroll effect

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+
 import "./nav.css";
 function Nav() {
+  const [show, handleShow] = useState(true);
+
+  const transitionNavBar = () =>
+    window.scrollY < 100 ? handleShow(true) : handleShow(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => {
+      window.removeEventListener("scroll", transitionNavBar);
+    };
+  }, []);
   return (
-    <div className="nav nav_black">
+    <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
           className="nav_logo"
